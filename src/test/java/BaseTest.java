45efd40ceb6565270f87c57001ce321e55937abb
JtesterPRO8 <joseph.petersen@testpro.io>
2024-04-21 14:41:24 -0500

    Homewrok19 delete playlist

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,80 +4,54 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.*;
-
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
 import java.time.Duration;
 
 public class BaseTest {
-
     public WebDriver driver;
-    //public String url = "https://qa.koel.app/";
-
-    /*@DataProvider(name="NegativeLoginTestData")
-    public Object[][] getDataFromDataProvider(){
-        return new Object[][]{
-                {"invalidEmail@class.com","invalid"},
-                {"demo@class.com","invalid"},
-                {"",""},
-                {"invalidEmail@class.com","te$t$tudent"},
-        };
-    }*/
+    public String url = "https://qa.koel.app/";
+    public String homePageUrl = "https://qa.koel.app/#!/home";
 
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
     }
 
-    /*@BeforeMethod
-    public void launchBrowser(){
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.manage().window().maximize();
-        navigateToPage();
-    }*/
-
     @BeforeMethod
     @Parameters({"BaseURL"})
-    public void launchBrowser(String baseURL){
+    public void launchBrowser(String BaseURL) {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
-        //String url = baseURL;
-        navigateToPage(baseURL);
+       // wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        url = BaseURL;
+        navigateToPage();
     }
 
+        public void closeBrowser() {
+            driver.quit();
+        }
+        public void clickSubmitBtn() {
+            WebElement submit = driver.findElement(By.cssSelector("button[type='submit']"));
+            submit.click();
+        }
+        public void providePassword(String password) {
+            WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
+            passwordField.clear();
+            passwordField.sendKeys(password);
+        }
+        public void provideEmail(String email) {
+            WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
+            emailField.clear();
+            emailField.sendKeys(email);
+        }
 
-    @AfterMethod
-    public void closeBrowser(){
-        driver.quit();
-    }
-
-    void clickLoginBtn() {
-        WebElement submit = driver.findElement(By.cssSelector("button[type='submit']"));
-        submit.click();
-    }
-
-    public void providePassword(String password) {
-        WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
-        passwordField.clear();
-        passwordField.sendKeys(password);
-    }
-
-    public void provideEmail(String email) {
-        WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
-        emailField.clear();
-        emailField.sendKeys(email);
-    }
-
-    /*public void navigateToPage() {
-        driver.get(url);
-    }*/
-
-    public void navigateToPage(String url) {
-        driver.get(url);
-    }
+        public void navigateToPage() {
+                driver.get(url);
+            }
 }
---
+++ src/test/java/Homework19.java
@@ -0,0 +1,45 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Homework19 extends BaseTest{
+    @Test
+    public void deletePlaylist() throws InterruptedException {
+        String expectedPlaylistDeleteMessage = "Deleted playlist \"test2.\"";
+
+        provideEmail("joseph.petersen@testpro.io");
+        providePassword("QaOneTwo!!!");
+        clickSubmitBtn();
+        Thread.sleep(2000);
+        openPlaylist();
+        Thread.sleep(2000);
+        clickDeletePlaylistBtn();
+        //clickOkToDelete();
+        Assert.assertEquals(getDeletePlaylistMsg(), expectedPlaylistDeleteMessage);
+    }
+
+    public void openPlaylist() {
+        WebElement playListElement = driver.findElement(By.xpath("//a[@href='#!/playlist/93988']"));
+        playListElement.click();
+    }
+
+    public void clickDeletePlaylistBtn() {
+        WebElement clickDeleteBtn = driver.findElement(By.cssSelector(".btn-delete-playlist"));
+        clickDeleteBtn.click();
+    }
+
+    public void clickOkToDelete(){
+       WebElement clickOkToDelete = driver.findElement(By.cssSelector("button[class='ok']"));
+       clickOkToDelete.click();
+    }
+
+    public String getDeletePlaylistMsg(){
+        //WebElement notificationMsg = driver.findElement(By.cssSelector("div.success.show"));
+        WebElement notificationMsg = driver.findElement(By.cssSelector("div[class='success show']"));
+        return notificationMsg.getText();
+    }
+
+
+}
